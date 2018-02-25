48117e6991d49f495c5855736b49b6f22ba75b80
Chaiwat <36126822+chaibluesky37@users.noreply.github.com>
2018-02-25 13:32:18 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ import Firebase from 'firebase';
 export default class App extends Component {
     componentWillMount() {
         Firebase.initializeApp({
-            apiKey: "AIzaSyDwj2DN9HBan5nUkdGJihorBG2eMZW-Kvc",
-            authDomain: "kmutnbpj3700.firebaseapp.com",
-            databaseURL: "https://kmutnbpj3700.firebaseio.com",
-             projectId: "kmutnbpj3700",
-            storageBucket: "kmutnbpj3700.appspot.com",
-            messagingSenderId: "298972134522"
+            apiKey: "xxx",
+            authDomain: "xxx",
+            databaseURL: "xxx",
+             projectId: "xxx",
+            storageBucket: "xxx",
+            messagingSenderId: "xxx"
         })
     }
     render() {
